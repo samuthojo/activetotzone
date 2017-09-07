4379da475cd1ee887ea61d211a8847e54802a299
samuthojo <samuthojo@gmail.com>
2017-09-07 17:44:04 +0300

    Added Create event functionality
    modified:   app/Http/Controllers/EventsController.php
    modified:   composer.json
    modified:   composer.lock
    modified:   config/app.php
    new file:   resources/views/events/event_form.blade.php

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -36,11 +36,39 @@ class EventsController extends Controller {
     }
 
     public function display_form() {
-      return view('cms.event_form');
+      return view('events.event_form');
     }
 
     public function create() {
-      
+      function saveThumb(Request $request){
+            $image = $request->file('image_url');
+
+            $destinationPath = public_path('images/events/');
+            $img = Image::make($image->getRealPath());
+            $new_image_name = time().'.'.$image->getClientOriginalExtension();
+            $img->save($destinationPath.$new_image_name,20);
+
+            $destinationPath = public_path('images/events/thumbs/');
+            $thumb_path = $destinationPath.$new_image_name;
+
+            $img->resize(rand (400, 800), null, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save($thumb_path,20);
+
+            return $new_image_name;
+        }
+
+        $event = [
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'date' => $request->input('date'),
+            'time' => $request->input('time'),
+            'venue' => $request->input('location'),
+            'link' => $request->input('link'),
+            'picture' => saveThumb($request),
+        ];
+
+        return Event::create($event);
     }
 
 }
