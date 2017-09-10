61e73c217a5ae91bebb4e97785307fec21bc672e
samuthojo <samuthojo@gmail.com>
2017-09-11 02:05:05 +0300

    Created books table schema
    Added the books migration files, the Book model and the seeder for the books table
    
    new file:   app/Book.php
    new file:   database/migrations/2017_09_10_224603_create_books_table.php
    new file:   database/migrations/2017_09_10_230128_EditBooksTable1.php
    new file:   database/seeds/BooksTableSeeder.php

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Book;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $faker = Faker\Factory::create();
+
+      for ($i=1; $i < 19; $i++) {
+          $event = [
+              'title' => $faker->sentence($faker->numberBetween(3, 5)),
+              'author' => $faker->name,
+              'description' => $faker->realText(120),
+              'cover_image' => "$i.jpg",
+              'url' => $faker->url(),
+          ];
+          Book::create($event);
+      }
+    }
+}
