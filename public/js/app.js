c97042d0f7898543b5bfc314c71e1dff035d5d42
Grayson Julius <graysonjulius@gmail.com>
2018-11-11 03:31:38 +0300

    Replace Instgram feeds with gallery

--- public/js/app.js
+++ public/js/app.js
@@ -963,7 +963,8 @@ module.exports = Cancel;
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(10);
-module.exports = __webpack_require__(42);
+__webpack_require__(42);
+module.exports = __webpack_require__(43);
 
 
 /***/ }),
@@ -43281,6 +43282,12 @@ if (false) {
 
 /***/ }),
 /* 42 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 43 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
